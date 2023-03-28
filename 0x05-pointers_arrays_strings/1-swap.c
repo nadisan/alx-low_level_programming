be49b9e9d46ef9fba8432fc66a2e1adb3e87ff94
Nadia <nadiawellela@gmail.com>
2023-03-28 11:08:56 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#inclue "main.h"
+/**
+ * Write a function that swaps the values of two integers.
+ * Prototype: void swap_int(int *a, int *b);
+ */
+void swap_int(int *a, int *b)
+{
+	int n = *a
+	int y = *b
+	*b = n
+	*a = y
+
+}
+	
