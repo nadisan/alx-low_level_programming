617dc2fe4929540f3d8f28757637ec30abeb2504
Nadia <nadiawellela@gmail.com>
2023-03-29 04:08:23 -0700

    codes

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strcat - returns the concatenates of two strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+}
