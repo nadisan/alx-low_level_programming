2ed82a182a7236f6e604a1d527102e57333df8e9
Nadia <nadiawellela@gmail.com>
2023-03-28 18:55:13 -0700

    strlen()

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen: function returns the length of a string
+ * @s: pointer to the string whose length is to be calculated
+ */
+void int _strlen(char *s)
+{
+        
+   strlen(*s)
+}
