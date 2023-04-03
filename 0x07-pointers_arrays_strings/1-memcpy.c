7fca5dcc962fa50832f68d9a6a8605af2eedc2c9
Nadia <nadiawellela@gmail.com>
2023-04-03 07:05:04 -0700

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int r = 0;
 	int i = n;
 
-	for (int r = 0; r < i; r++)
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
 }
-
 
