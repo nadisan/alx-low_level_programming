1b6568d6ad05be232ece0a305744981647a72558
Nadia <nadiawellela@gmail.com>
2023-04-04 23:53:14 -0700

    recrussion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "0-main.c"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *@s pointer to a string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+	return;
+}
