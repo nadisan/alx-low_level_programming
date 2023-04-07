b5787d815a4abfc712d843a98950367abb08f407
Nadia <nadiawellela@gmail.com>
2023-04-06 22:44:58 -0700

    codes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *
- *
+ *_puts_recursion - a function that prints a string, followed by a new line
+ *@s: the string to print
+ *Return: nothing.
  */
 void _puts_recursion(char *s)
 {
