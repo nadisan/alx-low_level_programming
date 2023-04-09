b5829bfd90950f40951e7c2c6542add73b4b0cb6
Nadia <nadiawellela@gmail.com>
2023-04-09 02:39:56 -0700

    codes

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *main -  program that adds positive numbers.
+ *@argc: count of the arguments supplied to the program
+ *@argv: pointer to  array of arguments
+ *Return: zero.
+ */
+
+int main(int argc, char **argv)
+{
+        int a, result;
+
+        result = 0;
+        if (argc == 1)
+        {
+                printf("0\n");
+                return (0);
+        }
+        while (argc > 1)
+        {
+                argc--;
+                if (*argv[argc] >= '0' && *argv[argc] <= '9')
+                {
+                        /* atoi convert a string to an int */
+                        a = atoi(argv[argc]);
+                        result = result + a;
+                }
+                else
+                {
+                        printf("Error\n");
+                        return (1);
+                }
+        }
+        printf("%d\n", result);
+        return (0);
+}
