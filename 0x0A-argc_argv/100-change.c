9bcbb0ec2b944df0d7e74a6a8aa751823ee23b64
Nadia <nadiawellela@gmail.com>
2023-04-09 12:07:13 -0700

    codes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include "cents.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- *main - minimum number of coins to make change for an amount of money
+ *main - minimum number of coins to make change for an amount of money.
  *@argc: count of the arguments supplied to the program
  *@argv: pointer to array of arguments
  *Return: zero.
@@ -23,38 +24,6 @@ int main(int argc, char **argv)
 	if (argc == 2)
 	{
 		check_rem(a);
-	}
-	int check_rem(int y);
-
-	int check_rem(int y)
-	{
-		int i, z;
-
-		int x[] = {25, 10, 5, 2, 1};
-
-		for (i = 0; i < 5; i--)
-		{
-			if (y >= x[i])
-			{
-				if (y % x[i] <= 2)
-				{
-					z = y / x[i] + y % x[i];
-					printf("%d\n", z);
-					return (0);
-				}
-				else
-				{
-					z = y / x[i] + check_rem(y % x[i - 1]);
-					printf("%d\n", z);
-					return (0);
-				}
-			}
-			if (y < x[i])
-			{
-				check_rem(y % x[i - 1]);
-			}
-		}
-		return (0);
 	}
 	return (0);
 }
