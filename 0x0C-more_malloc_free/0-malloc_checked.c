723c0b83a570617ce87f54b3bd3415587044445b
Nadia <nadiawellela@gmail.com>
2023-04-11 23:56:00 -0700

    codes

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/*
+ **malloc_checked - allocates memory using malloc.
+ *@b: unsigned intenger
+ *Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		return ("98");
+	return s;
+}
