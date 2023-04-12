26636506d2b92b1da92129fab94ffd9e645aef23
Nadia <nadiawellela@gmail.com>
2023-04-12 03:36:13 -0700

    codes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,20 +18,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	char *s;
 
-	if (s2 == NULL)
-	{
-		if (s1 == NULL)
-			return (NULL) ;
-		s2 = "";
-	}
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
+	if (s2 == NULL)
+        {
+		s2 = "";
+        }
 	y = strlen(s1);
 	s = malloc(sizeof(s1) + n + 1);
 	if (s == NULL)
-		return ("\0");
+		return (s = "NULL");
 	for (i = 0 ; i < (y + n) ; i++)
 	{
 		if (i < y)
