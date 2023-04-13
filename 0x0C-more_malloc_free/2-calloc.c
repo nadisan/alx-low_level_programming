03382f203b958649a6284a9f3e596d059303c118
Nadia <nadiawellela@gmail.com>
2023-04-13 01:37:23 -0700

    codes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,11 +15,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
 
+	unsigned int i;
+
+	arr = 0;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(size * nmemb + 1);
+	arr = mailloc(size * nmemb + 1);
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
 	return (arr);
 }
