c3c720c0ff20e0c473e357fb80a45140d8ff2bcb
Nadia <nadiawellela@gmail.com>
2023-04-13 23:27:47 -0700

    codes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,40 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *recur - check if integer n is prime number.
+ *@n: the given number to be checked if it's prime number.
+ *@y: number to divide n by.
+ *Return: 1 for prime number and 0 otherwise.
+ */
 
+int recur(int n, int y);
+
+int recur(int n, int y)
+{
+	if (n > y)
+	{
+		if (n % y == 0)
+			return (0);
+		y++;
+		recur(n, y);
+	}
+	return (1);
+}
 /**
  *is_prime_number - returns 1 for a prime number, otherwise return 0
  *@n: the input integer.
  *
- *Return: natural number or -1 for no natural square roots.
+ *Return: 1 for prime number and 0 otherwise.
  */
 
 int is_prime_number(int n)
 {
-	int i;
 
 	if (n < 2)
 		return (0);
-	for (i = 2; i < n; i++)
+	if (n == 2)
 	{
-		if (n % i == 0)
-			return (0);
+		return (1);
 	}
-	return (1);
+	return (recur(n, 2));
 }
