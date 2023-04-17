3f17091f4d2c3a01656b341641efa9ef083df010
Nadia <nadiawellela@gmail.com>
2023-04-17 01:23:35 -0700

    code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *struct dog - that has name, age and owner
+ *@name: string
+ *@age:	integer
+ *@owner: string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
