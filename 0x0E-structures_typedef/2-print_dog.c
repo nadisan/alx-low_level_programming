011a9f4b700ba1d3165fc3add7c7ecffa06f6dbd
Nadia <nadiawellela@gmail.com>
2023-04-18 02:48:22 -0700

    codes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ void print_dog(struct dog *d)
 		d->name = "nil";
 	if (d->owner == NULL)
 		d->owner = "nil";
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
