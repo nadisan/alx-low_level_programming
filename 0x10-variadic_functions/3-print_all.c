13e0ab9c658971180ca2e9790a8cffc8ab2ce6c7
Nadia <nadiawellela@gmail.com>
2023-04-23 07:28:55 -0700

    codes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,36 +17,33 @@ void print_all(const char * const format, ...)
 
 	va_start(list, format);
 	i = 0;
-	sp = " ";
+	sp = "";
 	if (format)
 	{
 		while (format[i] != '\0')
 		{
-			if (format[i + 0] == '\0')
-				sp = "";
 			switch (format[i])
 			{
 				case ('c'):
-						printf("%c%s", va_arg(list, int), sp);
+						printf("%s%c", sp,va_arg(list, int));
 						break;
 				case ('i'):
-						printf("%i%s", va_arg(list, int), sp);
+						printf("%s%i", sp, va_arg(list, int));
 						break;
 				case ('s'):
 						nil = va_arg(list, char *);
 						if (!nil)
-						{
 							nil = "(nil)";
-						}
-						printf("%s%s", nil, sp);
+						printf("%s%s", sp, nil);
 						break;
 				case ('f'):
-						printf("%f%s", va_arg(list, double), sp);
+						printf("%s%f", sp, va_arg(list, double));
 						break;
 				default:
 						break;
 			}
 			i++;
+			sp = ", ";
 		}
 	}
 	printf("\n");
