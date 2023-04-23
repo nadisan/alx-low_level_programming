9c6009a114e59ffb49ec42578d96c838cdf21113
Nadia <nadiawellela@gmail.com>
2023-04-22 19:36:56 -0700

    codes

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *print_numbers - returns the sum of all its parameters
+ *@separator: string to be printed between numbers
+ *@n: number of arguments
+ *
+ *Return: 0 when n is null else sum.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list ap;
+
+	i = 0;
+	va_start(ap, n);
+	while (i < n)
+	{
+		printf("%i", va_arg(ap, unsigned int));
+			if (separator != NULL)
+				printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+}
