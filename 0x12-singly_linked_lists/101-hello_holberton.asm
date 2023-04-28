373a822be8ad15f1146b599459e408ef6bb423fa
Nadia <nadiawellela@gmail.com>
2023-04-28 16:30:31 -0700

    101-hello_holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+
+	hello db 'Hello, Holberton',0Ah;
+	global main
+main:
+	;write "Hello, Holberton" to stdout
+	mov rax, 4;
+	mov rbx, 1;
+	mov rcx, hello;
+	mov rdx, 17;
+	int 0x80;
+
+	;
+	mov eax,1;
+	xor ebx,ebx;
+	int 0x80;
