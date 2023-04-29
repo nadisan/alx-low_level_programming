d4db872cdbf10b17bfce90ef2f6070200d3d71b6
Nadia <nadiawellela@gmail.com>
2023-04-28 22:58:44 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint: frees a listint_t list
+ * @head: listint_t list
+ * Return: Void.
+ */
+
+void free_listint(listint_t *head)
+{
+	if(head)
+		free_listint(head->next);
+	free(head);
+
+}
