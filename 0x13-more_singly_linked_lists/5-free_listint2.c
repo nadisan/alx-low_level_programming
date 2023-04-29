0d1ec102cefa84ce0abbae240e5a9cfb6fda6726
Nadia <nadiawellela@gmail.com>
2023-04-28 23:51:39 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: listint_t list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head;
+
+	if (node)
+	{
+		free_listint2(&(node->next));
+		free(node);
+	}
+	node = NULL;
+	*head = node;
+}
