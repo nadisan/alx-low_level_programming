cb48dfcd9692fa98dd714b9440ef1d8604a0aa28
Nadia <nadiawellela@gmail.com>
2023-05-02 08:45:09 -0700

    Revert "1"
    This reverts commit 4ccf63d3f80b0c63a8f0559f4d89e5f2762c7fc7.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,43 +15,37 @@ size_t print_listint_safe(const listint_t *head)
 	int i = 0, loop = 0;
 	const listint_t *tor, *hare;
 
-	if (head)
-	{	hare = head;
-		tor = head;
-		while (hare != NULL && hare->next != NULL)
-		{	tor = tor->next;
-			hare = hare->next->next;
-			if (tor == hare)
-			{	loop = 1;
-				break;
-			}
-		}
-		if (loop)
-		{	tor = head;
-			while (tor != hare)
-			{	tor = tor->next;
-				hare = hare->next;
-				printf("[%p] %i\n", (void *)tor, tor->n);
-				i++;
-			}
-			tor = tor->next;
-			while (tor != hare)
-			{	printf("[%p] %i\n", (void *)tor, tor->n);
-				tor = tor->next;
-				i++;
-			}
-			printf("-> [%p] %i\n", (void *)tor, tor->n);
-			return (i);
-		}
-		else
-		{
-			while (head)
-			{	printf("[%p] %i\n", (void *)head, head->n);
-				head = head->next;
-				i++;
-			}
-			return (i);
+	hare = head;
+	tor = head;
+	while (hare != NULL && hare->next != NULL)
+	{	tor = tor->next;
+		hare = hare->next->next;
+		if (tor == hare)
+		{	loop = 1;
+			break;
 		}
 	}
-	exit(98);
+	if (loop)
+	{	tor = head;
+		while (tor != hare)
+		{	tor = tor->next;
+			hare = hare->next;
+			printf("[%p] %i\n", (void *)tor, tor->n);
+			i++;
+		}
+		tor = tor->next;
+		while (tor != hare)
+		{	printf("[%p] %i\n", (void *)tor, tor->n);
+			tor = tor->next;
+			i++;
+		}
+		 printf("-> [%p] %i\n", (void *)tor, tor->n);
+	}
+	else
+		while (head)
+		{	printf("[%p] %i\n", (void *)head, head->n);
+			head = head->next;
+			i++;
+		}
+	return (i);
 }
