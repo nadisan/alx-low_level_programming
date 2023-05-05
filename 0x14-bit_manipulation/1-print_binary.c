b18f69856b9202f33fa7eb884d24947b0d573f03
Nadia <nadiawellela@gmail.com>
2023-05-04 19:49:25 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,15 +9,24 @@
 
 void print_binary(unsigned long int n)
 {
-	int x = 0;
+	unsigned long int x = 0;
+	unsigned long int y = 1UL <<(sizeof(unsigned long int)* 8 - 1);
 
-	if (n == 0 || n == 1)
-		printf("%li", n);
-	else
-		if (n)
-		{	x = n;
-			x -= (2 * (n >> 1));
-			print_binary(n >>= 1);
-			printf("%i", x);
+	while (y)
+	{
+		if (n & y)
+		{	putchar('1');
+			x = 1;
 		}
+		else if (x)
+		{
+			putchar('0');
+		}
+		y >>= 1;
+	}
+
+	if(!x)
+	{
+		putchar('0');
+	}
 }
