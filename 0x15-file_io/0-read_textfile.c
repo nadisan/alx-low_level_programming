63201185d2e2058325a68097b36e1ad5eb49f9b5
Nadia <nadiawellela@gmail.com>
2023-05-10 01:06:11 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standout
+ *@filename: name of file to read
+ *@letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{	int c;
+	int i = letters;
+	FILE *file;
+
+	if (!filename || !letters)
+		return (0);
+	if (!(fopen(filename, "r")))
+		return (0);
+	file = fopen(filename, "r");
+
+	while (i)
+	{
+		c = fgetc(file);
+		if (feof(file))
+			break;
+		printf("%c", c);
+		i--;
+	}
+	fclose(file);
+	return (letters);
+}
