4428f521239ad2ced0e6d08f539c0d375f4833e9
Nadia <nadiawellela@gmail.com>
2023-05-11 07:24:00 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,33 @@
  * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-{	char c;
-	size_t i = 0;
-	ssize_t j;
+{
+	char *buffer;
+	ssize_t b_read, b_write;
+	int fd;
 
-	FILE *file;
-
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
-	if (!(fopen(filename, "r")))
+	fd = open(filename, O_RDONLY);
+	if (!(fd))
 		return (0);
-	file = fopen(filename, "r");
 
-	while (i < letters)
-	{
-		c = fgetc(file);
-		if (feof(file))
-			break;
-		j = write(1, &c, 1);
-		if (j == -1)
-			return (0);
-		i++;
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{	close(fd);
+		return (0);
 	}
-	fclose(file);
-	return (i);
+	b_read = read(fd, buffer, letters);
+	if (b_read != -1)
+	{
+		b_write = write(STDOUT_FILENO, buffer, b_read);
+		if (b_write != -1)
+		{	close(fd);
+			free(buffer);
+			return (b_write);
+		}
+	}
+	close(fd);
+	free(buffer);
+	return (0);
 }
