c8f0a0b4dde43a8ac373ee4f211f1b6aac77207d
Nadia <nadiawellela@gmail.com>
2023-05-12 05:24:29 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * cp_file - copies the content of a file to another file
+ * @file_1: file to be copied.
+ * @file_2: file to copy to.
+ * Return: void.
+ */
+
+void cp_file(const char *file_1, const char *file_2)
+{
+	int fd1, fd2;
+	size_t size;
+	char *buffer;
+	ssize_t b_read, b_write;
+	struct stat st;
+
+	if (!file_1 || !file_2)
+		return;
+	fd1 = open(file_1, O_RDONLY);
+
+	if (fd1 == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s.\n", file_1);
+		exit(98);
+	}
+	fd2 = open(file_2, O_CREAT | O_RDWR);
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't write to %s.\n", file_2);
+		exit(99);
+	}
+	stat(file_1, &st);
+	size = st.st_size;
+	buffer = malloc(sizeof(char) * size);
+	b_read = read(fd1, buffer, size);
+	if (b_read != -1)
+	{
+		b_write = write(fd2, buffer, b_read);
+		if (b_write != -1)
+		{	close(fd1);
+			close(fd2);
+			free(buffer);
+			return;
+		}
+	}
+	close(fd1);
+	close(fd2);
+	free(buffer);
+}
+
+
+
+/**
+ * main - check the code
+ * @ac: number of arguments passed
+ * @av: array of arguments.
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp_file(av[1], av[2]);
+	return (0);
+}
+
+
