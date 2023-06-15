9d696792534dae0baf5b3c3866aa8d5b99ebfa84
Nadia <nadiawellela@gmail.com>
2023-06-15 06:22:40 -0700

    print_dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,9 +13,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		printf ("%i\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 		i++;
 	}
