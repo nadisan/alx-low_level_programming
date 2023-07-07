8ca9912e58202e18de9a439545d49ed518483432
Nadia <nadiawellela@gmail.com>
2023-07-06 20:22:57 -0700

    hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,16 @@
  * size:  size of the array
  * Return: a pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h = malloc(sizeof(unsigned long int) * (size));
+	hash_table_t *h = malloc(sizeof(hash_table_t));
 
 	if (h == NULL)
 		return (NULL);
 	h->size = size;
+	h->array = malloc(sizeof(hash_node_t *) * size);
+	if (h->array == NULL)
+		return (NULL);
 	return (h);
 }
