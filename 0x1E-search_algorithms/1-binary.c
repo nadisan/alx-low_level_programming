1df0f37484510eeaa16292a9dbe8d422f69caed6
nadisan <nadiawellela@gmail.com>
2023-10-10 10:46:06 -0700

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 			if (h == (size - 1))
 				printf("%lu\n", h);
 			else
-				printf("%lu,", h);
+				printf("%lu, ", h);
 		}
 		h = (i + size - 1)/2;
 		if (array[h] == value)
