5208ddeb974cc6cfecda9f801ffcea7ac1f76128
nadisan <nadiawellela@gmail.com>
2023-10-11 03:42:06 -0700

    100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * jump_search - find a value in sorted array of integers w jump search
+ * @array:  pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * Return: the index with the value or -1
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (-1);
+}
