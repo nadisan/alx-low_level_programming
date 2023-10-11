e5c61366e212026d05dc9ea9872c49ccd0e3d0e4
nadisan <nadiawellela@gmail.com>
2023-10-11 07:59:56 -0700

    Null

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,7 +16,10 @@ int binary_search(int *array, size_t size, int value)
 	size_t h;
 
 	if (!array)
+	{	printf("Searching in array: \n");
 		return (-1);
+	}
+
 	while (i < size)
 	{
 		printf("Searching in array: ");
